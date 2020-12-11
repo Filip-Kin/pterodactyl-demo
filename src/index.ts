98091a169b60c94d84faa7074559e37ee4f1557c
Filip Kin <filip@kinmails.com>
2020-12-10 20:30:26 -0500

    magic

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as colors from 'colors';
+import { exit } from 'process';
+
+console.log('Hello, world!'.red.bold);
+console.log('This is the example app for the pterodactyl panel');
+console.log('Note that it downloads a repository, builds the app, and then runs it automatically');
+setTimeout(() => {
+    console.log('Look at the files tab and you\'ll find all the app\'s files which you can edit through this panel');
+    console.log('Note however if you have repository pulling enabled you might run into merge issues if you change files in there');
+    console.log('Of course you could also just upload your app manually either through the panel or the FTP server');
+}, 1000);
+setTimeout(exit, 5000)
