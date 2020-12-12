664c7176e35a07f9cd763d9dc8544c687d1e04a6
Filip Kin <filip@kinmails.com>
2020-12-12 10:54:05 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 const colors = require('colors/safe');
 const process = require('process');
 
-console.log(colors.red.bold('Hello, world!'));
-console.log('This is the example app for the pterodactyl panel');
-console.log('Note that it downloads a repository, builds the app, and then runs it automatically');
-setTimeout(() => {
-    console.log('Look at the files tab and you\'ll find all the app\'s files which you can edit through this panel');
-    console.log('Note however if you have repository pulling enabled you might run into merge issues if you change files in there');
-    console.log('Of course you could also just upload your app manually either through the panel or the FTP server');
-}, 1000);
-setTimeout(process.exit, 5000)
+let messages = [
+    colors.green.bold('Hello, world!'),
+    'This is the example app for the pterodactyl panel',
+    'Note that it downloads a repository, builds the app, and then runs it automatically',
+    'Look at the files tab and you\'ll find all the app\'s files which you can edit through this panel',
+    'Note however if you have repository pulling enabled you might run into merge issues if you change files in there',
+    'Of course you could also just upload your app manually either through the panel or the FTP server',
+    'This demo application will continue running until the scheduled exit task runs (every 5 minutes)'
+];
+
+const out(i) => {
+    if (!(i < messages.length)) return;
+    console.log(messages[i]);
+    setTimeout(() => out(i++), 500);
+};
+
+// don't exit for atleast 5 minutes to avoid auto restarting
+setTimeout(() => {}, 60e3*5);
