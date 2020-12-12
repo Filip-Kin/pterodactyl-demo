206c568e9fdab8877e74412c22595414d3ec533b
Filip Kin <filip@kinmails.com>
2020-12-12 10:47:32 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const colors = require('colors');
+const colors = require('colors/safe');
 const process = require('process');
 
-console.log('Hello, world!'.red.bold);
+console.log(colors.red.bold('Hello, world!'));
 console.log('This is the example app for the pterodactyl panel');
 console.log('Note that it downloads a repository, builds the app, and then runs it automatically');
 setTimeout(() => {
@@ -9,4 +9,4 @@ setTimeout(() => {
     console.log('Note however if you have repository pulling enabled you might run into merge issues if you change files in there');
     console.log('Of course you could also just upload your app manually either through the panel or the FTP server');
 }, 1000);
-setTimeout(exit, 5000)
+setTimeout(process.exit, 5000)
